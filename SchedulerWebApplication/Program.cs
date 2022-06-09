2a94debe0a462fbe9e3f5ea9edb05251850cf42d
mateusz7812 <mateusz78122@gmail.com>
2022-06-09 21:44:05 +0200

    env variables

--- SchedulerWebApplication/Program.cs
+++ SchedulerWebApplication/Program.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
@@ -23,6 +24,7 @@ namespace SchedulerWebApplication
                 })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    webBuilder.UseUrls($"http://*:{Environment.GetEnvironmentVariable("LISTENING_PORT") ?? "80"}");
                     webBuilder.UseStartup<Startup>();
                 });
     }
